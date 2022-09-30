0c2ae70fb903b5868aa5293d7270fd3f79512c1f
fxf981 <105899270+fxf981@users.noreply.github.com>
2022-09-30 11:24:02 +0800

    Create f.sh

---
+++ f.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+wget -N https://raw.githubusercontent.com/fxf981/koCaddy/main/xr
+chmod +x xr
+# Set config file
+cat <<EOF >/fei.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": 8080,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "d8b6af50-11dd-46ae-b598-6701778ce6b3",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": false
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+nohup node ./xr -config=/fei.json >/dev/null 2>/dev/null &
