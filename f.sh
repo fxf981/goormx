610578531c41b81189f8a00006bd9c2bc6ff73e8
fxf981 <105899270+fxf981@users.noreply.github.com>
2022-10-02 22:53:04 +0800

    Create f.sh

---
+++ f.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+wget -N https://raw.githubusercontent.com/fxf981/koCaddy/main/xr
+chmod +x xr
+# Set config file
+cat <<EOF >/fei.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": 9999,
+            "protocol": "shadowsocks",
+            "settings": {
+                "method": "aes-128-gcm",
+                "password": "goorm"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+nohup ./xr -config=/fei.json >/dev/null 2>/dev/null &
